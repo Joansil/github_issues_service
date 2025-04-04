c12f8aa327befca3a86d0a32b51987986eab917d
Joansil <joansilfs@gmail.com>
2025-04-04 16:17:39 -0300

    refactor controller to use fallback

--- lib/github_issues_service_web/controllers/repository_controller.ex
+++ lib/github_issues_service_web/controllers/repository_controller.ex
@@ -4,56 +4,29 @@ defmodule GithubIssuesServiceWeb.RepositoryController do
 
   alias GithubIssuesService.Repositories.Repository
 
-  def create(conn, %{"user" => user, "repository" => repository}) do
-    clean_repository = repository |> String.replace("https://github.com/", "") |> String.replace(~r/.*\//, "")
+  action_fallback GithubIssuesServiceWeb.FallbackController
 
-    Logger.info("Fetching data for user: #{user}, repository: #{clean_repository}")
-
-    case Repository.fetch_repository_data(user, clean_repository) do
-      {:ok, _data} ->
-        conn
-        |> put_status(:accepted)
-        |> json(%{
-          message: "Request accepted, webhook will be sent in 24 hours",
-          details: %{
-            user: user,
-            repository: clean_repository
-          }
-        })
-
-      {:error, :not_found} ->
-        conn
-        |> put_status(:not_found)
-        |> json(%{
-          error: "Repository not found",
-          details: [
-            "Please ensure that:",
-            "1. The repository exists",
-            "2. The repository is public",
-            "3. The user name is correct",
-            "4. The repository name is correct (don't include the full GitHub URL)"
-          ],
-          example: %{
-            correct_format: %{
-              user: "username",
-              repository: "repository-name"
-            },
-            example_request: %{
-              user: "phoenixframework",
-              repository: "phoenix"
-            }
-          }
-        })
-
-      {:error, reason} ->
-        Logger.error("Error processing request: #{inspect(reason)}")
-        conn
-        |> put_status(:internal_server_error)
-        |> json(%{
-          error: "An error occurred",
-          message: "#{inspect(reason)}",
-          details: "Please check the request format and try again"
-        })
+  @spec create(Plug.Conn.t(), map()) :: Plug.Conn.t()
+  def create(conn, params) do
+    with {:ok, user, repository} <- validate_params(params),
+         {:ok, _data} <- Repository.fetch_repository_data(user, repository) do
+      conn
+      |> put_status(:accepted)
+      |> json(%{
+        message: "Request accepted, webhook will be sent in 24 hours",
+        details: %{
+          user: user,
+          repository: repository
+        }
+      })
     end
+  end
+
+  defp validate_params(%{"user" => user, "repository" => repository}) when is_binary(user) and is_binary(repository) do
+    {:ok, user, repository}
+  end
+
+  defp validate_params(_) do
+    {:error, :missing_fields}
   end
 end
